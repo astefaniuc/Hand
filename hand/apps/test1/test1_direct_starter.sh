3a747d0fe68486d58696dc77c01d74ce9e89788b
Alex Stefaniuc <alex.stefaniuc@googlemail.com>
2012-07-25 11:41:29 +0200

    Moved README up to be visible on the base project page on github. Adde script to start hand.

--- hand/apps/test1/test1_direct_starter.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./../../toolkit
-../../dyntkserver_starter ~/workspace/test_app/apps/test1/test1.so
---
+++ hand/hand.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./toolkit
+./handserver_starter $1
