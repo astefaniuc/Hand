7acadf909d0dec2888614e1cff6e59717f49fad3
Alex Stefaniuc <alex.stefaniuc@googlemail.com>
2012-10-02 19:38:40 +0200

    Remove std::vector polution + erase(Vertex) method

---
+++ hand/toolkit/graph/baselist.h
@@ -0,0 +1,50 @@
+/*
+ *  Copyright 2012 Alex Stefaniuc
+ *
+ *  This file is part of Hand.
+ *
+ *  Hand is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation, either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  Hand is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with Hand. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GRAPH_BASELIST_H
+#define GRAPH_BASELIST_H
+
+#include <vector>
+
+
+class Vertex;
+
+
+typedef std::vector<Vertex*>::iterator VIterator;
+
+
+class BaseList : public std::vector<Vertex*>
+{
+    public:
+        using vector::erase;
+        bool erase(Vertex* v)
+        {
+            VIterator curr;
+            for(curr=begin(); curr!=end(); curr++)
+                if((*curr) == v)
+                {
+                    erase(curr);
+                    return true;
+                }
+
+            return false;
+        };
+};
+
+#endif /* GRAPH_BASELIST_H */
